fa7685c5647a00f75d027ba107aaf83af5bb38bb
devCJH <http-@naver.com>
2024-11-01 16:56:28 +0900

    jsp, jstl세팅, main.jsp

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,14 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	
 	@GetMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
+		return "usr/home/main";
+	}
+	
+	@GetMapping("/")
+	public String showMain2() {
+		return "redirect:/usr/home/main";
 	}
 }
